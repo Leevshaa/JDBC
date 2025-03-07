cc8a0aef864eb6041922fe86139fca55a6767ccd
Vitalii <vitaliti.yurchenko@gmail.com>
2025-03-07 22:58:13 +0200

    Added database connection functionality

---
+++ src/main/java/app/DatabaseConnector.java
@@ -0,0 +1,18 @@
+package app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/company";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "B74p3KdR";
+
+    //System.getenv("DB_PASSWORD");
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
